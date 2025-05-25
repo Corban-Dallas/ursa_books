3844704217b4a6f2ec1d96f13759bd5f7220925a
ASUS-ROG-F17\grish <grisha992@gmail.com>
2025-05-25 12:01:24 +0700

    windows ui thread setup fix

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -17,11 +17,11 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   // plugins.
   ::CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
   flutter::DartProject project(L"data");
+  project.set_ui_thread_policy(flutter::UIThreadPolicy::RunOnPlatformThread);
 
   std::vector<std::string> command_line_arguments =
       GetCommandLineArguments();
 
-  project.set_ui_thread_policy(UIThreadPolicy::RunOnPlatformThread);
   project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
 
   FlutterWindow window(project);
